084fe9fb7dd77bb633f70dda5dca659a61e36b15
DimmArt <dimchik.xai@gmail.com>
2016-08-15 16:07:38 +0300

    Commands help updates

--- index.rb
+++ index.rb
@@ -7,11 +7,12 @@ file = ''
 
 OptionParser.new do |opts|
   opts.banner = "Usage: index.rb [options]"
-  opts.on('-f', '--file PATH', 'File path')   { |val| file = val }
+  opts.on('-f', '--file PATH', 'File name')   { |val| file = val }
 end.parse!
 
 if file.empty?
   puts 'No file to parse provided'
+  puts 'Please try index.rb -h to see the available options'
 else
   if !File.exist?(file)
     puts "File `#{file}` doesn\'t exist"
@@ -20,14 +21,3 @@ else
     IndexFileParser.process_file(file)
   end
 end
-
-# puts options
-# words = parse_file(options[:file_path])
-#
-# if words.size > 0
-#   words.each do |word|
-#     if word.size > 1
-#       add_index(word, options[:file_path])
-#     end
-#   end
-# end
--- search.rb
+++ search.rb
@@ -6,9 +6,15 @@ search_for = ''
 
 OptionParser.new do |opts|
   opts.banner = "Usage: search.rb [options]"
-  opts.on('-s', '--search STRING', '"Search string"', String) { |val| search_for = val }
+  opts.on('-s', '--search STRING', 'use quotes for multi word query', String) { |val| search_for = val }
 end.parse!
 
 
-puts "Looking for '#{search_for}':"
-puts SearchIndex.find(search_for)
+if search_for.empty?
+  puts 'No search query provided'
+  puts 'Please try search.rb -h to see the available options'
+else
+  puts "Looking for '#{search_for}':"
+  puts SearchIndex.find(search_for)
+end
+
